bc22baf3f69888bc3cab310d156982b64d797591
lifejuggler <jtchoiboy@gmail.com>
2016-02-14 23:27:01 -0500

    first push

---
+++ application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/cc0ba23f630fca5b/f28fd9e4e442d3c9.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_76fc30ae0e65872a1d34f4187142b6e4_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1455683016;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1455264460;
---
+++ application/themes/tasty/elements/footer.php
@@ -0,0 +1,35 @@
+<?php defined('C5_EXECUTE') or die("Access Denied.");
+
+$footerSiteTitle = new GlobalArea('Footer Site Title');
+$footerSocial = new GlobalArea('Footer Social');
+$footerSiteTitleBlocks = $footerSiteTitle->getTotalBlocksInArea();
+$footerSocialBlocks = $footerSocial->getTotalBlocksInArea();
+$displayFirstSection = $footerSiteTitleBlocks > 0 || $footerSocialBlocks > 0 || $c->isEditMode();
+
+?>
+
+<footer id="footer-theme">
+    <?php if ($displayFirstSection) { ?>
+    <section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-9">
+                <?php
+                $a = new GlobalArea('Footer Site Title');
+                $a->display();
+                ?>
+            </div>
+            <div class="col-sm-3">
+                <?php
+                $a = new GlobalArea('Footer Social');
+                $a->display();
+                ?>
+            </div>
+        </div>
+    </div>
+    </section>
+    <?php } ?>
+</footer>
+
+
+<?php $this->inc('elements/footer_bottom.php');?>
