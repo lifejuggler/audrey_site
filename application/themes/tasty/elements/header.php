f689c3a2bbe90b31b39de8d0585df61fe14b29f6
lifejuggler <jtchoiboy@gmail.com>
2016-05-11 18:46:28 -0400

    fixed header

--- application/themes/tasty/elements/header.php
+++ application/themes/tasty/elements/header.php
@@ -9,26 +9,24 @@ $displayThirdColumn = $blocks > 0 || $c->isEditMode();
 <header>
     <div class="container">
         <div class="row">
-            <div class="col-lg-4 col-sm-5 col-xs-12">
+            <div class="col-sm-4 col-sm-5 col-xs-12">
                 <?php
                 $a = new GlobalArea('Header Site Title');
                 $a->display();
                 ?>
             </div>
-            <div class="navbar-section <?php if ($displayThirdColumn) { ?>col-lg-6 col-sm-9 col-xs-3<?php } else { ?>col-sm-9 col-xs-3<?php } ?>">
+            <div class="navbar-section col-sm-6 col-sm-9 col-xs-3">
                 <?php
                 $a = new GlobalArea('Header Navigation');
                 $a->display();
                 ?>
             </div>
-            <?php if ($displayThirdColumn) { ?>
-                <div class="col-lg-2 col-sm-2 col-xs-9">
-                    <?php
-                    $a = new GlobalArea('Book Now');
-                    $a->display();
-                    ?>
-                    </div>
-            <?php } ?>
+            <div class="col-sm-2 col-xs-9">
+                <?php
+                $a = new GlobalArea('Book Now');
+                $a->display();
+                ?>
+            </div>
         </div>
     </div>
 </header>
